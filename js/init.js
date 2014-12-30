f05a22cedc961d05cabe19c016af180c4b4efc40
osquatro <x166tzxl>
2014-12-30 12:46:11 +0000

    added separated init file

---
+++ js/init.js
@@ -0,0 +1,59 @@
+var context, canvas;
+(function () {
+    window.requestAnimFrame = (function (callback) {
+        return window.requestAnimationFrame
+            || window.webkitRequestAnimationFrame
+            || window.mozRequestAnimationFrame
+            || window.oRequestAnimationFrame
+            || window.msRequestAnimationFrame
+            || function (callback) {
+                callback();
+            };
+    })();
+
+    canvas = document.getElementById(GameSettings.CanvasId);
+    context = canvas.getContext('2d');
+
+    var controller = new TankController(context);
+
+    var processor = new BackgroundProcessor({
+        timeout: GameSettings.fps,
+        currentGameState: null, // {LOADING, PLAY, FINISH}
+        startProcessingCallBack: function () {
+            controller.loadLevel(Level_1);
+            this.currentGameState = GameState.LOADING;
+        },
+        stopConditionCallBack: function () {
+            return this.currentGameState === GameState.FINISH;
+        },
+        workerCallBack: function () {
+            switch (this.currentGameState) {
+                case GameState.LOADING :
+                    if (controller.isLoadingCompleted()) {
+                        controller.gameCreate();
+                        this.currentGameState = GameState.PLAY;
+                    }
+                    break;
+                case GameState.PLAY :
+                    requestAnimFrame(function () {
+                        controller.doAction1();
+                    });
+                    break;
+                default: // do nothing so far
+                    break;
+            }
+        }
+    });
+
+    document.getElementById('btnStart').onclick = function () {
+        processor.start();
+    };
+
+    document.getElementById('btnPause').onclick = function () {
+        processor.interrupt();
+    };
+
+    document.getElementById('btnResume').onclick = function () {
+        processor.resume();
+    };
+})();
